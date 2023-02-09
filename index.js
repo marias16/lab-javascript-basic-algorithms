6a88e1055686c12698c527ea274550affba5256a
Maria Suarez <msuarezh@gmail.com>
2023-02-09 21:22:29 +0100

    bonus 1 lorem ipsum done

--- index.js
+++ index.js
@@ -37,3 +37,21 @@ if (hacker1 < hacker2) {
 } else {
     console.log("What?! You both have the same name");
 }
+
+// bonus
+
+//lorem ipsum
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac commodo mauris. Cras pretium tempus venenatis. Vestibulum at egestas enim. Integer vel viverra elit. Ut iaculis risus vitae lacus condimentum vulputate. Donec dapibus aliquet dui sit amet fermentum. Ut at dignissim sapien, in maximus urna. Fusce lectus felis, luctus eget porttitor vel, pharetra vel turpis. Maecenas scelerisque aliquet turpis, sed pharetra massa cursus maximus. Nulla facilisi. Praesent nec massa eleifend, molestie nibh in, commodo ex. Integer in lacus ipsum. Mauris luctus massa arcu, sit amet suscipit magna dignissim nec. Aliquam ut ex lobortis massa posuere consectetur. Pellentesque ut massa risus. Pellentesque eget orci vel nibh suscipit lobortis vulputate ut lacus. Quisque commodo magna sed turpis fermentum, ac varius eros rutrum. Morbi venenatis congue orci, vitae pulvinar turpis scelerisque sit amet. Aenean ullamcorper pellentesque diam, vel scelerisque augue ultrices eget. Fusce id nisl ut turpis porttitor fermentum. Maecenas efficitur enim tempus ligula iaculis, sollicitudin porta felis sodales. Donec vel velit id arcu pellentesque convallis efficitur non diam. Morbi faucibus sagittis leo, vel rutrum felis accumsan eu. Duis sodales nulla vitae nisi molestie suscipit. Fusce pellentesque ante ut orci ultrices posuere. Fusce ullamcorper congue blandit. Donec vehicula justo at maximus hendrerit. Nunc fermentum vitae odio in egestas. Fusce dapibus feugiat libero, sit amet tristique sapien malesuada nec. Cras eu massa luctus libero volutpat posuere ut vel dolor. Vivamus a faucibus velit. Quisque eget ornare ex. Maecenas quis pretium sapien, non convallis dui. Integer eget metus non nisl tincidunt vestibulum. Integer malesuada dolor sed fermentum posuere. Morbi volutpat at metus nec cursus. Aliquam euismod nibh vitae nisi feugiat accumsan. Nam tincidunt ipsum nec sapien tincidunt, vitae vestibulum sapien facilisis. Aliquam imperdiet ac augue sed tincidunt. Proin egestas varius arcu, in euismod augue tristique nec. Donec facilisis gravida elit in aliquet. Phasellus dolor purus, convallis sit amet ullamcorper eu, tincidunt sit amet massa."
+
+let wordsCounter = 0;
+for (let i=0; i<longText.length; i++) {
+  if(longText[i] === " ") {
+    wordsCounter++;
+  } else {
+    continue;
+  }
+}
+
+console.log(wordsCounter)
+
+//
